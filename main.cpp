97886e1319272cb4459b776e6d75703f90cc3f38
moisesmtzs <moises.martinez6605@alumnos.udg.mx>
2020-09-29 00:52:12 -0500

    Agregado if para cada caso solicitado e impresión de cada caso en bytes

--- main.cpp
+++ main.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+
+    string opcion;
+    getline( cin, opcion );
+    if ( opcion == "char" ){
+        cout<<sizeof(char)<<endl;
+    } else if ( opcion == "int" ){
+        cout<<sizeof(int)<<endl;
+    } else if ( opcion == "short" ){
+        cout<<sizeof(short)<<endl;
+    } else if ( opcion == "float" ){
+        cout<<sizeof(float)<<endl;
+    } else if ( opcion == "double" ){
+        cout<<sizeof(double)<<endl;
+    } else if ( opcion == "long" ){
+        cout<<sizeof(long)<<endl;
+    } else {
+        cout<<"Bye"<<endl;
+    }
 
     return 0;
 }
